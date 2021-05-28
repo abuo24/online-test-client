7e3aa8a80044f69110308235d428e6cd092da77f
Orifjon Yunusjonov <mryediniofficial9924@gmail.com>
2021-05-28 21:53:58 +0500

    test-select change

--- src/server/host.js
+++ src/server/host.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import {getToken} from '../utilits';
 import {tokenKey} from '../Components/constants/constants';
 
-// export let host='https://online-test-web-app.herokuapp.com';
-export let host='https://educational-center-web-app.herokuapp.com';
+export let host='https://online-test-web-app.herokuapp.com';
+// export let host='https://educational-center-web-app.herokuapp.com';
 
 export let httpRequest=(config)=>{
     return axios({
